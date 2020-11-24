b7897862e6e34252b411651611651775afe0c791
Felipe Marini <felipemarini71188@gmail.com>
2020-11-24 16:10:34 -0300

    Algorithm with few things to finish

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ex7_ListaFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[15];
+
+            for (var i = 0; i <= 14; i++) {
+                
+                Console.WriteLine();
+                Console.WriteLine($"Digite o {i+1}° número:\n ");
+                numeros[i] = double.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sua lista com os números na ordem invertida em que digitou é:\n ");
+            
+            for (var i = 14; i >= 0; i--) {
+                
+
+                Console.WriteLine();
+
+                Console.WriteLine(numeros[i]);
+
+                
+
+            }
+            
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
+                
+        
+
+
+                
+
+
+
